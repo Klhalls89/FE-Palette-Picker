bf565c33657758126045693a1bd4458702809bfa
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-04 15:47:18 -0600

    Setup variables

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Logo">Palette Picker</h1>
+        <h1 className="logo">Palette Picker</h1>
       </div>
     )
   }
