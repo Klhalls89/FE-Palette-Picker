09440eab561e1f98f898468d14c6c85d44485947
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-05 20:29:05 -0600

    Begin wireframe

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,6 +12,12 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="logo">Palette Picker</h1>
+        <section className="main-colors">
+          <div className="color-1">
+            <p>#FD3754</p>
+            <i className="fas fa-lock"></i>
+          </div>
+        </section>
       </div>
     )
   }
