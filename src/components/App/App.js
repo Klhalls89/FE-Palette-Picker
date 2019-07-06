fd161a39c1a31f973bd160b5d8b55df0b45c8c0c
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-05 21:04:05 -0600

    Complete wire frame

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -43,6 +43,21 @@ class App extends Component {
           <input type="text"/>
           <button className="save-pal">Save Palette</button>
         </form>
+        <form className="project-form">
+          <input type="text"/>
+          <button>Project Name</button>
+        </form>
+        <section className="saved-portion">
+          <article className="project-area">
+            <h2>project name</h2>
+            <h3>palette name</h3>
+            <div className="saved-color1"></div>
+            <div className="saved-color2"></div>
+            <div className="saved-color3"></div>
+            <div className="saved-color4"></div>
+            <div className="saved-color5"></div>
+          </article>
+        </section>
       </div>
     )
   }
