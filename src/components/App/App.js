3c6093558a3a55797f860a86b76c073c841e7fbe
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-06 21:35:13 -0600

    Get colors displaying

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,21 +5,22 @@ class App extends Component {
     super()
     this.state = {
       colors: []
-    }
-  }
+    };
+  };
 
   componentDidMount(){
-    this.genHex()
-  }
+    this.makeFiveColors()
+  };
 
   makeFiveColors = () => {
-    let newColors = []
+    let colors = []
     for (let i = 0; i < 5; i++) {
       let color = this.genHex()
-      newColors.push(color)
+      colors.push(color)
     }
-    this.setState({colors: newColors})
-  }
+    this.setState({colors})
+  };
+
   genHex = () => {
     let hexables = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F' ];
     let shuffledHexables = hexables.sort(() => Math.random() - 0.5)
@@ -27,37 +28,37 @@ class App extends Component {
         finalColor += hexable
         return finalColor.slice(0, 6);
     }, '')
-    return `# ${newColor}`;
-  }
+    return `#${newColor}`;
+  };
 
   render() {
-    console.log(this.state.colors)
+    const {colors} = this.state
     return (
       <div className="App">
         <h1 className="logo">Palette Picker</h1>
         <section className="main-colors">
-          <div className="color-1">
-            <p>#FD3754</p>
+          <div style={{backgroundColor: colors[0]}} className="color-1">
+            <p>{colors[0]}</p>
             <i className="fas fa-lock"></i>
           </div>
-          <div className="color-2">
-            <p>#FB1754</p>
+          <div style={{backgroundColor: colors[1]}} className="color-2">
+            <p>{colors[1]}</p>
             <i className="fas fa-lock"></i>
           </div>
-          <div className="color-3">
-            <p>#0B1754</p>
+          <div style={{backgroundColor: colors[2]}} className="color-3">
+            <p>{colors[2]}</p>
             <i className="fas fa-lock"></i>
           </div>
-          <div className="color-4">
-            <p>#0B1B54</p>
+          <div style={{backgroundColor: colors[3]}} className="color-4">
+            <p>{colors[3]}</p>
             <i className="fas fa-lock"></i>
           </div>
-          <div className="color-5">
-            <p>#FB1B33</p>
+          <div style={{backgroundColor: colors[4]}} className="color-5">
+            <p>{colors[4]}</p>
             <i className="fas fa-lock"></i>
           </div>
         </section>
-        <button className="gen-colors">Generate Colors</button>
+        <button onClick={() => this.makeFiveColors()} className="gen-colors">Generate Colors</button>
         <form className="color-form">
           <select>
             <option>project 1</option>
