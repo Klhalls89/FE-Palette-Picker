e381cc4644cacbf35e4e7aedcf4ae2f04b8569ad
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-07 12:01:50 -0600

    Add guts to projects component

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -11,7 +11,21 @@ class Projects extends Component {
   render() {
     return (
       <div className="projects">
-   
+        <form className="project-form">
+          <input type="text"/>
+          <button>Project Name</button>
+        </form>
+        <section className="saved-portion">
+          <article className="project-area">
+          <h2>project name</h2>
+          <h3>palette name</h3>
+          <div className="saved-color1"></div>
+          <div className="saved-color2"></div>
+          <div className="saved-color3"></div>
+          <div className="saved-color4"></div>
+          <div className="saved-color5"></div>
+          </article>
+        </section>
       </div>
     )
   }
