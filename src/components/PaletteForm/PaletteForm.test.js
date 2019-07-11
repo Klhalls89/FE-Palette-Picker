9b4852eac4de53660b6965bd16c7f0aa42d6ae65
Kristen Hallstrom <klhalls89@gmail.com>
2019-07-10 22:53:35 -0600

    Palette Form test

---
+++ src/components/PaletteForm/PaletteForm.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PaletteForm from './PaletteForm.js'
+import { shallow } from 'enzyme';
+const shortid = require('shortid')
+
+
+describe('PaletteForm', () => {
+  let wrapper;
+  let mockProjects;
+  let mockHandleProject;
+
+  beforeEach(() => {
+    let mockHandleProject = jest.fn()
+     mockProjects = [{project_title: 'project one'}]
+    wrapper = shallow(<PaletteForm projects={mockProjects} 
+                                  handleProject={mockHandleProject}/>)
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  });
+
+    describe('options', () => {
+      it('should return an array of options', () => {
+        wrapper.instance().options()
+        expect.arrayContaining(<option></option>)
+      });
+    });
+});
